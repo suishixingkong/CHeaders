e6d4c335636827f4cfbbedab80da77fd83a4f92c
Leonardo Esparis <leoxnidas.c.14@gmail.com>
2015-10-03 12:28:24 -0430

    better enhancement

--- CHeaders.py
+++ CHeaders.py
@@ -33,44 +33,100 @@ class CompleteCHeaders(sublime_plugin.EventListener):
         self._include_wrapper_files = "#include <%s>"
         self._include_wrapper_dirs = "#include <%s/"
 
-        self._cpp_libs = []
-        self._cpp_paths = []
-        self._cpp_versions = ["4.9", "4.8", "4.7"]
+        if self.is_linux:
 
-        if sublime.platform() == "linux":
-            for _p in self._paths:
-                for v in self._cpp_versions:
-                    if os.path.exists(_p + "c++/" + v):
-                        for _f in glob.glob(_p + "c++/" + v + "/*"):
-                            if os.path.isdir(_f):
-                                _caption = _f.rsplit("/", 1)[1]
-                                _r = self._include_wrapper_dirs % _caption
-                                self._cpp_libs.append(
-                                    (_caption + "\tdirectory", _r)
-                                )
-                            elif os.path.isfile(_f):
-                                _caption = _f.rsplit("/", 1)[1]
-                                _r = self._include_wrapper_files % _caption
-                                self._cpp_libs.append(
-                                    (_caption + "\tmodule", _r)
-                                )
+            # looking for c++ headers
+            # as iostream, deque, vector, list
+            # in linux systems
+            self._cpp_gnu_h = {}
+            self._cpp_path = "/usr/include/c++/"
+            self._cpp_supported_versions = ["4.7", "4.8", "4.9"]
+            self._cpp_version = None
+
+            for v in self._cpp_supported_versions:
+                if os.path.exists(self._cpp_path + v):
+                    self._cpp_version = v
                     break
+
+            for _f in glob.glob(self._cpp_path + self._cpp_version + "/*"):
+                if os.path.isdir(_f):
+                    _caption = _caption = _f.rsplit("/", 1)[1]
+                    self._cpp_gnu_h.update(
+                        {_caption: "directory"}
+                    )
+                elif os.path.isfile(_f):
+                    _caption = _f.rsplit("/", 1)[1]
+                    self._cpp_gnu_h.update(
+                        {_caption: "module"}
+                    )
+            self._cpp_path += v + "/"
+
+            # looking for linux gnu headers
+            # as sys/socket.h ...
+            self._linux_gnu_h = {}
+            self._linux_gnu_path = "/usr/include/i386-linux-gnu/"
+
+            for _f in glob.glob(self._linux_gnu_path + "*"):
+                if os.path.isdir(_f):
+
+                    _caption = _f.replace(self._linux_gnu_path, "")
+
+                    self._linux_gnu_h.update(
+                        {_caption: "directory"}
+                    )
+                if os.path.isfile(_f):
+
+                    _caption = _f.replace(self._linux_gnu_path, "")
+
+                    self._linux_gnu_h.update(
+                        {_caption: "module"}
+                    )
+
+    @property
+    def is_linux(self):
+        return sublime.platform() == "linux"
+
+    @property
+    def is_window(self):
+        return sublime.sublime.platform() == "window"
 
     @property
     def _view(self):
-        return self._window.active_view()
-
-    @property
-    def _window(self):
-        return sublime.active_window()
+        return sublime.active_window().active_view()
 
     @staticmethod
-    def _parse_response():
-        return ("_caption", "_result")
+    def _find_substring(view, location):
+        for x in range(location):
+            _sub = view.substr(sublime.Region(location - (x + 1), location))
+            if _sub.startswith(">\n"):
+                return view.substr(
+                    sublime.Region(location - (x + 1) + len(">\n"),
+                                   location)
+                )
+        return view.substr(
+            sublime.Region(0, location)
+        )
 
-    @staticmethod
-    def _find_substring(viewsubstr, location):
-        pass
+    def _is_main_path(self):
+        for _sp in self._settings_paths:
+            if _sp in self._paths:
+                return True
+        return
+
+    def _parse_result(self, substr, header, type):
+        _caption = header + '\t' + type
+        if type == "directory":
+            if "#include" not in substr:
+                _r = self._include_wrapper_dirs % header
+            else:
+                _r = header + "/"
+        if type == "module":
+            if "#include" not in substr:
+                _r = self._include_wrapper_files % header
+            else:
+                _r = header + ">"
+
+        return _caption, _r
 
     def on_query_context(self, view, key, operator, operand, match_all):
         pass
@@ -83,63 +139,76 @@ class CompleteCHeaders(sublime_plugin.EventListener):
 
         if re.search(regex, file_name):
 
-            for x in range(location[0]):
-                subs = view.substr(
-                    sublime.Region(location[0] - (x + 1), location[0]))
-                if subs.startswith(">\n"):
-                    substr = view.substr(
-                        sublime.Region(location[0] - ((x + 1) - len(">\n")),
-                                       location[0]))
-                    break
-
+            substr = self._find_substring(view, location[0])
             rx = re.search(r"(\w+(-*\.*\w+/*|/\w+)*(?=/))", substr)
 
             if rx:
+                self._paths = []
+                start = rx.start()
+                end = rx.end()
+
                 for i, _p in enumerate(self._settings_paths):
-                    self._paths[i] = _p + substr[rx.start():rx.end()] + '/'
+                    if os.path.exists(_p + substr[start:end] + '/'):
+                        self._paths.append(_p + substr[start:end] + '/')
+
+                # adding cpp headers path to self._paths
+                if os.path.exists(self._cpp_path + substr[start:end] + '/'):
+                    self._paths.append(
+                        self._cpp_path + substr[start:end] + '/')
+
+                # if system is linux
+                # add linux-gnu headers path to self._paths
+                if self.is_linux:
+                    _f = self._linux_gnu_path + substr[start:end] + '/'
+                    if os.path.exists(_f):
+                        self._paths.append(_f)
+
             else:
                 self._paths = []
                 for _p in self._settings_paths:
                     self._paths.append(_p)
 
-            for _p in self._settings_paths:
-                if _p in self._paths:
-                    result += self._cpp_libs
-                    break
+            # if the file extention is .c
+            # result will not have c++ standart libraries...
+            if not re.search(r"\.c$", file_name):
+                if self._is_main_path():
+                    for _p in self._cpp_gnu_h:
+                        _r = self._parse_result(
+                            substr,
+                            _p,
+                            self._cpp_gnu_h[_p])
 
-            print(self._paths, prefix, substr)
+                        result.append(_r)
+
+            # adding linux gnu libs
+            if self._is_main_path() and self.is_linux:
+                for _p in self._linux_gnu_h:
+                    _r = self._parse_result(
+                        substr,
+                        _p,
+                        self._linux_gnu_h[_p])
+
+                    result.append(_r)
+
             for path in self._paths:
                 _glob_result = glob.glob(path + prefix + "*")
                 if _glob_result:
                     for item in _glob_result:
                         if item.replace(path, "").startswith(prefix):
                             if os.path.isdir(item):
-                                for _p in self._settings_paths:
-                                    if _p in item:
-                                        _caption = item.replace(
-                                            _p, "")
-                                        break
-                                _result = copy.deepcopy(_caption)
-                                if "#include" not in substr:
-                                    _result = self._include_wrapper_dirs % _result
-                                if not _result.endswith("/"):
-                                    _result = _result + "/"
-                                _caption += "\tdirectory"
-                                result.append((_caption,
-                                               _result))
+                                _r = self._parse_result(
+                                    substr,
+                                    item.replace(path, ""),
+                                    "directory"
+                                )
+                                result.append(_r)
                             elif os.path.isfile(item):
-                                _caption = item.replace(path, "") + "\tmodule"
-                                _i = item.replace(path, "")
-
-                                if "include" not in _i and \
-                                        "include" not in substr:
-                                    _i = self._include_wrapper_files % _i
-
-                                if not _i.endswith(">") and \
-                                        not substr.endswith(">"):
-                                    _i += ">"
-
-                                result.append((_caption, _i))
+                                _r = self._parse_result(
+                                    substr,
+                                    item.replace(path, ""),
+                                    "module"
+                                )
+                                result.append(_r)
 
         return result
 
